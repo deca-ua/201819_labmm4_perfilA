8da1981eadffb29a823d7ce284d47cdcf743c797
csantosnapraia <carlos.m.santos@gmail.com>
2019-02-13 21:37:07 +0000

    Estrutura de pastas para exercício

---
+++ trabalho/web/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab4 - Carlos Santos</title>
+</head>
+<body>
+<h1>Carlos Santos</h1>
+<p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis.
+    Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum.
+    Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+</body>
+</html>
