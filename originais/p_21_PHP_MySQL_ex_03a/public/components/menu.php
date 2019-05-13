3a829a941f29ad3959001e09b3d236e3058b931e
Pedro Beça <PedroBeca@ua.pt>
2019-05-13 12:23:31 +0100

    Exercício PHP MYSQL 03a correcção no componente "menu.php"

--- originais/p_21_PHP_MySQL_ex_03a/public/components/menu.php
+++ originais/p_21_PHP_MySQL_ex_03a/public/components/menu.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav ml-auto">
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../admin/">Admin</a></li>
     <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" {username}<span
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> {username}<span
                 class="caret"></span></a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
             <li><a class="dropdown-item" href="scripts/logout.php">Logout</a></li>
