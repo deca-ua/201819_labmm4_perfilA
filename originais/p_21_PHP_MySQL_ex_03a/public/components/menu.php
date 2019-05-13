00ff86e90574e87c8751cbbe8235252b497bd70b
Pedro Beça <PedroBeca@ua.pt>
2019-05-13 09:56:09 +0100

    Exercício PHP MYSQL 03a

---
+++ originais/p_21_PHP_MySQL_ex_03a/public/components/menu.php
@@ -0,0 +1,14 @@
+<ul class="navbar-nav ml-auto">
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../admin/">Admin</a></li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" {username}<span
+                class="caret"></span></a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="scripts/logout.php">Logout</a></li>
+        </ul>
+    </li>
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#login">Login</a></li>
+    <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+    </li>
+</ul>
